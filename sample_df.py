f79b4a0dff4363bad702b21988fdc8af7ca2d761
Indra <emailindra04@gmail.com>
2022-03-10 16:23:07 +0530

    Modified sample_df.py to connect cpanel db

--- sample_df.py
+++ sample_df.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from sqlalchemy import create_engine
+#import pymysql
 
 def df_to_table(hostname,dbname,uname,pwd,table_name,df):
 
     engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
-                    .format(host=hostname, db=dbname, user=uname, pw=pwd))
+                    .format(host=hostname, db=dbname, user=uname, pw=pwd ))
 
+
+                    
     df.to_sql(con=engine,name=table_name,if_exists='append',index=False)
     print('db updated successfully')
 
@@ -13,13 +16,20 @@ def df_to_table(hostname,dbname,uname,pwd,table_name,df):
 
 if __name__ =='__main__':
 
-    hostname="localhost"
-    dbname="test"
-    uname="root"
-    pwd="root"
+    # hostname="localhost"
+    # dbname="test"
+    # uname="root"
+    # pwd="root"
+    # table_name='student'
+
+    hostname="184.168.119.128"
+    dbname="educodb"
+    uname="educo"
+    pwd="admin"
     table_name='student'
 
     student_df=pd.read_csv("C:/Users/i.e.bhattacharya/Downloads/Educo/sample_df.csv")
+    # print(student_df)
     df_to_table(hostname,dbname,uname,pwd,table_name,student_df)
 
 
